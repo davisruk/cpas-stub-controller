a2b1cc5d2a8e2e43e398c5e1db516bc5e6196685
Richard Davis <rich@davisfamily.eu>
2020-11-03 09:57:38 +0000

    Removed comment

--- src/app/dashboard/custom-breakpoints.ts
+++ src/app/dashboard/custom-breakpoints.ts
@@ -11,12 +11,12 @@ const MY_BREAKPOINTS = [
     { alias: 'lt-md', mediaQuery: 'screen and (max-width: 991px)' },
     { alias: 'lt-lg', mediaQuery: 'screen and (max-width: 1199px)' },
     { alias: 'lt-xl', mediaQuery: 'screen and (max-width: 1669px)' },
-    // { alias: 'lt-xl', mediaQuery: 'screen and (max-width: 1919px)' },
 
     { alias: 'gt-xs', mediaQuery: 'screen and (min-width: 768px)' },
     { alias: 'gt-sm', mediaQuery: 'screen and (min-width: 992px)' },
     { alias: 'gt-md', mediaQuery: 'screen and (min-width: 1200px)' },
-    { alias: 'gt-lg', mediaQuery: 'screen and (min-width: 1670px)' },];
+    { alias: 'gt-lg', mediaQuery: 'screen and (min-width: 1670px)' },
+];
 
 export const CustomBreakPointsProvider = {
     provide: BREAKPOINT,
